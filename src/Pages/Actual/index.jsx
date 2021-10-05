c85569f2065608839ef3d28d26a9c306de7065fa
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2021-10-06 01:16:28 +0500

    design almost complete for figma

--- src/Pages/Actual/index.jsx
+++ src/Pages/Actual/index.jsx
@@ -19,6 +19,11 @@ const Actual = () => {
         'Cash - User Defined2',
         'Cash - User Defined3',
         'Cash - Petty Cash']);
+
+    const [labels, setLabels] = useState([]);
+
+    const [currentLabel, setCurrentLabel] = useState("");
+
     const [currentOption, setCurrentOption] = useState("");
     const [option, setOption] = useState("");
 
@@ -34,7 +39,8 @@ const Actual = () => {
     // const location = useLocation();
     // const { pathname } = location;
     useEffect(() => {
-
+        const label = ["A", "B", "C", "D", "E", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+        setLabels(label);
         //////////////////////////////////////////////////////////////////////////////////////////Retreiving firebase data from web
         // Now retreiving the data
         //////////////////////////////Here all data retreiving is working////////////////////////////
@@ -62,7 +68,7 @@ const Actual = () => {
         //         console.log(`data for current selected user `, convertArrayToObject(data, 1));
         //     })
         //////////////////////////////////////////////////////////////////////////////////////////Retreiving firebase data from web
-    })
+    }, [])
 
     const pushAvailableOptions = () => {
         //Either I have to insert the new element or not.I will check if already a option present it cant be inserted again.
@@ -89,7 +95,8 @@ const Actual = () => {
         if (debit != 0 && currentOption != "") {
             let obj = {
                 selectedOption: currentOption,
-                Debit: debit
+                Debit: debit,
+                Label: currentLabel
             }
             setAvailableDebits([...availableDebits, obj]);
             setDebit(0);
@@ -100,13 +107,17 @@ const Actual = () => {
         if (currentOption == "") {
             alert("Please select an option also from the above drop down to submit debit value.")
         }
+        if (currentLabel == "") {
+            alert("Please select a Label from the Menu to Submit the value.")
+        }
     }
 
     const pushAvailableCredits = () => {
         if (credit != 0 && currentOption != "") {
             let obj = {
                 selectedOption: currentOption,
-                Credit: credit
+                Credit: credit,
+                Label: currentLabel
             }
             setAvailableCredits([...availableCredits, obj]);
             setCredit(0);
@@ -116,6 +127,9 @@ const Actual = () => {
         }
         if (currentOption == "") {
             alert("Please select an option also from the above drop down to submit Credit value.")
+        }
+        if (currentLabel == "") {
+            alert("Please select a Label from the Menu to Submit the value.")
         }
     }
 
@@ -223,8 +237,195 @@ const Actual = () => {
                             </div>
                             <div className="tab-pane fade" id="ex3-tabs-2" role="tabpanel" aria-labelledby="ex3-tab-2">
                                 <div className="container">
-                                    <div className="row border">
+                                    <div className="row">
+                                        <div className="container">
+                                            <br />
+                                            <h4 className="text-dark text-head">Select the account name from the drop down list located below
+                                                <p className="text-danger text-bold text-center mt-4"> <b>OR</b></p> Input desired account names by navigating to the Add Values Tab located on the left side bar:
+                                                {/* :  <span className="text-danger ml-4">*</span></h4> */}
+                                            </h4>
+                                            <div title="Select the account name from the drop down list located below" className="input-group input-group-md txt-field">
+                                                <span className="input-group-addon glyphicon glyphicon-search" id="sizing-addon2"></span>
+                                                <select style={{ fontSize: "18px", width: "200px", height: "40px" }} value={currentOption}
+                                                    onChange={(e) => setCurrentOption(e.target.value)} className="form-control">
+                                                    {["No Selected Value", ...availableOptions].map((v, i) => {
+                                                        return <option value={v} key={i}>
+                                                            {v}
+                                                        </option>
+                                                    })}
+                                                </select>
+                                            </div>
 
+                                            <br />
+
+
+                                            <h4 className="text-primary text-bold">
+                                                Select the label you want to insert with the value :
+                                            </h4>
+
+                                            <div className="input-group input-group-md txt-field">
+                                                <span className="input-group-addon glyphicon glyphicon-search" id="sizing-addon2"></span>
+                                                <select style={{ fontSize: "18px", width: "200px", color: "blue" }} value={currentLabel}
+                                                    onChange={(e) => setCurrentLabel(e.target.value)} className="form-control">
+                                                    {["No Selected Label", ...labels].map((v, i) => {
+                                                        return <option value={v} key={i}>
+                                                            {v}
+                                                        </option>
+                                                    })}
+                                                </select>
+                                            </div>
+
+                                            <br />
+                                            <br />
+
+                                        </div>
+
+                                        <hr />
+
+                                        <div className="container">
+                                            <div className="row">
+                                                <div className="col-md-6">
+                                                    <h3 className="text-primary mt-3 mb-3">Debit Amount : -</h3>
+                                                    {/* The Drop down for selecting the option  */}
+                                                    <input type="number" placeholder="Enter any debit amount eg: '100' " className="form-control txt-field" value={debit} onChange={(e) => setDebit(e.target.value)} />
+                                                    <br />
+                                                    <button className="btn btn-primary btn-push" onClick={() => pushAvailableDebits()}>Add Debit Amount</button>
+                                                    <br />
+                                                    <br />
+                                                </div>
+                                                <div className="col-md-6">
+                                                    <h3 className="text-primary mt-3 mb-3">Credit Amount : -</h3>
+                                                    {/* The Drop down for selecting the option  */}
+                                                    <input type="number" placeholder="Enter any credit amount eg: '10' " className="form-control txt-field" value={credit} onChange={(e) => setCredit(e.target.value)} />
+                                                    <br />
+                                                    <button className="btn btn-primary btn-push" onClick={() => pushAvailableCredits()}>Add Credit Amount</button>
+                                                    <br />
+                                                    <br />
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <br />
+
+                                        <div className="container">
+                                            <div className="border w-fit-content">
+                                                <div className="row">
+                                                    <div className="col-md-12">
+                                                        <div className="input-group input-group-md category_select txt-field">
+                                                            <span className="input-group-addon glyphicon glyphicon-search"></span>
+                                                            <select id="Table-DropDown" value={currentOption}
+                                                                onChange={(e) => setCurrentOption(e.target.value)} className="form-control">
+                                                                {["No Selected Value", ...availableOptions].map((v, i) => {
+                                                                    return <option value={v} key={i}>
+                                                                        {v}
+                                                                    </option>
+                                                                })}
+                                                            </select>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className="row">
+                                                    <div className="col-md-6">
+                                                        <div className="table">
+                                                            <table className="table table-hover">
+                                                                <thead>
+                                                                    <tr>
+                                                                        <th colSpan={3}>
+                                                                            <h5 className="text-bold"><i className="fas fa-list-alt fa-lg mr-3 text-success"></i>&nbsp;&nbsp; Debit Card</h5>
+                                                                            <h6><span className="text-success">The amount in Credit Card in different sections</span> </h6>
+                                                                        </th>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <th scope="col"> <p> <b>Label</b></p> </th>
+                                                                        <th scope="col"> <p><b>Amounts</b></p></th>
+                                                                        <th scope="col"> <p><b>Edit</b></p> </th>
+                                                                    </tr>
+                                                                </thead>
+                                                                <>
+
+                                                                    <tbody>
+
+                                                                        {(availableDebits.length == 0) ? (
+                                                                            <tr>
+                                                                                <th scope="row">No Label</th>
+                                                                                <th scope="row" >No Value</th>
+                                                                                <th scope="row"><button className="btn btn-warning btn-sm" disabled={true}>E</button></th>
+                                                                            </tr>
+                                                                        ) : (
+                                                                            availableDebits.map((v, j) => {
+                                                                                return <tr key={j}>
+
+                                                                                    <>
+                                                                                        <th scope="row" className="text-center w-fit-content text-bold mt-3"><h6 className="text-bold">{v.Label}</h6></th>
+
+                                                                                        <td>
+                                                                                            <h6 className="text-success mt-2 text-center text-bold">{v.Debit}</h6>
+                                                                                        </td>
+
+                                                                                        <td className="text-center"><button type="button" className="btn btn-warning btn-sm">E</button></td>
+                                                                                    </>
+
+                                                                                </tr>
+                                                                            })
+                                                                        )}
+                                                                    </tbody>
+                                                                    {/* This matters */}
+                                                                </>
+                                                            </table>
+                                                        </div>
+                                                    </div>
+                                                    <div className="col-md-6">
+                                                        <div className="table">
+                                                            <table className="table table-hover">
+                                                                <thead>
+                                                                    <tr>
+                                                                        <th colSpan={3}>
+                                                                            <h5 className="text-bold"><i className="fas fa-list-alt fa-lg mr-3 text-danger"></i>&nbsp;&nbsp; Credit Card</h5>
+                                                                            <h6><span className="text-danger">The amount in Credit Card in different sections</span> </h6>
+                                                                        </th>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <th scope="col"> <p> <b>Label</b></p> </th>
+                                                                        <th scope="col"> <p><b>Amounts</b></p></th>
+                                                                        <th scope="col"> <p><b>Edit</b></p> </th>
+                                                                    </tr>
+                                                                </thead>
+                                                                <>
+
+                                                                    <tbody>
+
+                                                                        {(availableDebits.length == 0) ? (
+                                                                            <tr>
+                                                                                <th scope="row">No Label</th>
+                                                                                <th scope="row" >No Value</th>
+                                                                                <th scope="row"><button className="btn btn-warning btn-sm" disabled={true}>E</button></th>
+                                                                            </tr>
+                                                                        ) : (
+                                                                            availableDebits.map((v, j) => {
+                                                                                return <tr key={j}>
+
+                                                                                    <>
+                                                                                        <th scope="row" className="text-center w-fit-content text-bold mt-3"><h6 className="text-bold">{v.Label}</h6></th>
+
+                                                                                        <td>
+                                                                                            <h6 className="text-success mt-2 text-center text-bold">{v.Debit}</h6>
+                                                                                        </td>
+
+                                                                                        <td className="text-center"><button type="button" className="btn btn-warning btn-sm">E</button></td>
+                                                                                    </>
+
+                                                                                </tr>
+                                                                            })
+                                                                        )}
+                                                                    </tbody>
+                                                                    {/* This matters */}
+                                                                </>
+                                                            </table>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
